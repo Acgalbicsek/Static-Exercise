673ac8d70259e5eb91bee6059fead6e7b3bb578a
Amy Galbicsek <acgalbicsek@gmail.com>
2025-02-27 12:45:57 -0900

    updated work

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace StaticExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace StaticExercise
 {
-    public class Program
+    public class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            var celsius = TempConverter.FToC(15);
+            Console.WriteLine($"{celsius} celsius");
+           
+
+            var farenheit = TempConverter.CToF(87);
+            Console.WriteLine($"{farenheit} farentheit");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter 
+    {
+
+        public static double FToC(double C)
+        {
+            return (C - 32) * 5 / 9;
+
+
+        }
+        
+
+        public static double CToF(double F)
+        {
+
+           
+            var result = (F * 9 / 5) + 32;
+            return result;
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
